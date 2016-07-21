4095e012d77d2d1117a78adf04bcb671ee8e14f9
Alin Voinea <alin@eaudeweb.ro>
2016-07-21 16:14:15 +0300

    Refs #74574 - Fix setup-env.py for recovery.conf

--- postgres/docker-entrypoint.sh
+++ postgres/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-/bin/setup-env.py
 /bin/crond.sh
 
 if [ -z "$POSTGRES_REPLICATE_FROM" ]; then
+	/bin/setup-env.py
 	exec /master-entrypoint.sh "$@"
 else
 	exec /replica-entrypoint.sh "$@"
