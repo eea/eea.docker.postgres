51c3b32f2ae1282d5a3df17af30afea96a25d2c0
Alin Voinea <alin@eaudeweb.ro>
2017-02-24 17:21:14 +0200

    Refs #80360 - Fix setup-schema and setup-permissions
    - Add possibility to restrict replication network

--- postgres/setup-permissions.sh
+++ postgres/setup-permissions.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# Restrict replication only on POSTGRES_REPLICATION_NETWORK
+# e.g.:
+#   POSTGRES_REPLICATION_NETWORK="172.16.0.0/12"
+#
+if [ ! -z "$POSTGRES_REPLICATION_NETWORK" ]; then
+  ALREADY_FIXED=$( grep "$POSTGRES_REPLICATION_NETWORK" /var/lib/postgresql/data/pg_hba.conf || true )
+  if [ -z "$ALREADY_FIXED" ]; then
+    echo "host replication all $POSTGRES_REPLICATION_NETWORK trust" >> /var/lib/postgresql/data/pg_hba.conf
+  fi
+else
+    # Docker network
+    echo "host replication all 172.16.0.0/12 trust" >> /var/lib/postgresql/data/pg_hba.conf
 
-# Docker network
-echo "host replication all 172.17.0.0/16 trust" >> /var/lib/postgresql/data/pg_hba.conf
-
-# Rancher network
-echo "host replication all 10.42.0.0/16 trust" >> /var/lib/postgresql/data/pg_hba.conf
+    # Rancher network
+    echo "host replication all 10.0.0.0/8 trust" >> /var/lib/postgresql/data/pg_hba.conf
+fi
