8a13c65b0d619089c596f72dabd2be90fa794ab0
Alin Voinea <alin@eaudeweb.ro>
2017-04-25 12:40:42 +0300

    Fix permissions on PG_ARCHIVE, PG_BACKUP and PG_CONFD

--- postgres/docker-entrypoint.sh
+++ postgres/docker-entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 /bin/crond.sh
 
+mkdir -p PG_ARCHIVE $PG_CONFD $PG_RESTORE $PG_BACKUP
+chown -R postgres:postgres $PG_ARCHIVE $PG_CONF $PG_RESTORE $PG_BACKUP
+
 if [ -z "$POSTGRES_REPLICATE_FROM" ]; then
 	/bin/setup-env.py
 	exec /master-entrypoint.sh "$@"
