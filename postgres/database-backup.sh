8fe95397c290cc7a516ceb398e683f4e8b256b69
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-10-05 12:32:29 +0300

    Add possibility to specify backup/restore gzip path

--- postgres/database-backup.sh
+++ postgres/database-backup.sh
@@ -2,10 +2,15 @@
 set -e
 
 DB=$1
+GZ=$2
+
+if [ -z "${GZ}" ]; then
+    GZ="/postgresql.backup/${DB}.gz"
+fi
 
 if [ -z "${DB}" ]; then
-  echo "Usage gosu postgres /postgres.restore/database-backup.sh mydb"
+  echo "Usage gosu postgres /postgres.restore/database-backup.sh mydb [/path/to/mydb.gz]"
   exit 1
 fi
 
-pg_dump ${DB} | gzip > /postgresql.backup/${DB}.gz
+pg_dump ${DB} | gzip > ${GZ}
