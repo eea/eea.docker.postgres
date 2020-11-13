fefded7ffd2509209e275bb8f090502931fb8403
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-11-13 22:31:32 +0200

    fix sed command

--- postgres/setup-postgres.sh
+++ postgres/setup-postgres.sh
@@ -6,5 +6,5 @@ set -e
 ALREADY_FIXED=$( grep include_dir.*postgresql.conf.d $PGDATA/postgresql.conf || true )
 if [ -z "$ALREADY_FIXED" ]; then
   echo "Updating $PGDATA/postgresql.conf"
-  sed -i "s#include_dir = .*#include_dir = '/postgresql.conf.d'#g" $PGDATA/postgresql.conf
+  sed -i "s#.*include_dir = .*#include_dir = '/postgresql.conf.d'#g" $PGDATA/postgresql.conf
 fi
