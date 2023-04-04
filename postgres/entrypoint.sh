1c67d9ea8220dbef2dcf14cceaddbfe833f2a148
valentinab25 <valentinab25>
2023-04-04 12:53:43 +0300

    Fix stretch bug

--- postgres/entrypoint.sh
+++ postgres/entrypoint.sh
@@ -8,7 +8,7 @@ chown -R postgres:postgres $PG_ARCHIVE $PG_CONF $PG_RESTORE $PG_BACKUP
 
 if [ -z "$POSTGRES_REPLICATE_FROM" ]; then
 	/bin/setup-env.py
-	exec /master-entrypoint.sh "$@"
+	exec master-entrypoint.sh "$@"
 else
-	exec /replica-entrypoint.sh "$@"
+	exec replica-entrypoint.sh "$@"
 fi
