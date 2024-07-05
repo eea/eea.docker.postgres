3ec24bd8bc5d17fac6e97507bc6149872054a7cc
daniel.popescu <daniel.popescu@bluedesk.ro>
2024-07-05 10:08:11 +0000

    postgress 16.3

--- postgres/setup-replication.sh
+++ postgres/setup-replication.sh
@@ -4,12 +4,16 @@ set -e
 echo "Running setup replication"
 
 function setup_default_recovery {
-touch /var/lib/postgresql/data/standby.signal
-cat > /postgresql.conf.d/zenvreplica.conf <<EOF
+  touch /var/lib/postgresql/data/standby.signal
+  cat > /postgresql.conf.d/zenvreplica.conf <<EOF
 primary_conninfo = 'host=$POSTGRES_REPLICATE_FROM port=5432 user=$POSTGRES_USER'
-promote_trigger_file = '/tmp/touch_me_to_promote_to_me_master'
 EOF
 
+}
+
+# Function to promote the standby server
+function promote_standby {
+  pg_ctl promote -D /var/lib/postgresql/data
 }
 
 # POSTGRES_REPLICATE_FROM env present
@@ -18,6 +22,11 @@ if [ ! -z "$POSTGRES_REPLICATE_FROM" ]; then
     # This instance is still a replica (was not promoted to master)
     if [ ! -f "/tmp/touch_me_to_promote_to_me_master" ]; then
       setup_default_recovery
+    fi
+  else
+    # Check if we need to promote this standby server
+    if [ -f "/tmp/touch_me_to_promote_to_me_master" ]; then
+      promote_standby
     fi
   fi
 fi
